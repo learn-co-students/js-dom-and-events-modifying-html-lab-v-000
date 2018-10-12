f329999ddde45a98c94f4b0068d49412da7cdc4f
sgibson47 <sgibson47@gmail.com>
2018-10-12 13:13:48 -0500

    Done.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+  //adding a string containing an html image element that displays image held at address the src attribute is set to
+  $("#image").append("<img id='theImg' src='https://s3.amazonaws.com/learn-verified/painting-with-dog.gif'/>");
+});
+
+
